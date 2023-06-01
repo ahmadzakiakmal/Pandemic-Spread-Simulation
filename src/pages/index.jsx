b58c5138e55d0e1a1a415d97adfcdab2724a739e
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-01 13:57:01 +0700

    feat: menu component with no functionality yet

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { IoPerson } from "react-icons/io5";
-import {BsFillGearFill} from "react-icons/bs";
+import { BsFillGearFill } from "react-icons/bs";
 
 function GridBox({ value, showVisuals, index, grid, cols }) {
   return (
@@ -34,6 +34,57 @@ function GridBox({ value, showVisuals, index, grid, cols }) {
   );
 }
 
+function Settings({ saveSettings }) {
+  return (
+    <div className="fixed w-screen h-screen bg-black/50 grid place-items-center">
+      <div className="bg-white py-5 px-10 w-[50%] rounded-[5px]">
+        <h1 className="text-center font-bold">Settings</h1>
+        <form
+          onSubmit={(e) => saveSettings(e)}
+          className="my-3 flex flex-col gap-2"
+        >
+          <div className="flex flex-col">
+            <label htmlFor="populationDensity">Population Density</label>
+            <input
+              className="!outline-none"
+              type="number"
+              name="populationDensity"
+              id="populationDensity"
+            />
+            <hr />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="infectionRate">Infection Rate</label>
+            <input
+              className="!outline-none"
+              type="number"
+              name="infectionRate"
+              id="infectionRate"
+            />
+            <hr />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="spreadRange">Spread Range</label>
+            <input
+              className="!outline-none"
+              type="number"
+              name="spreadRange"
+              id="spreadRange"
+            />
+            <hr />
+          </div>
+          <button
+            type="submit"
+            className="bg-black text-white rounded-md py-2 mt-5 hover:bg-black/90"
+          >
+            SAVE
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
 export default function Home() {
   // * Setup Grid
   const [rows, setRows] = useState(5);
@@ -55,6 +106,7 @@ export default function Home() {
 
   // * Variables for Visual Needs
   const [showVisuals, setShowVisuals] = useState(true);
+  const [showSettings, setShowSettings] = useState(false);
 
   // * Utility Variables
   const [refresh, setRefresh] = useState(false);
@@ -154,6 +206,13 @@ export default function Home() {
     }
   }, [iteration]);
 
+  // * Save Settings
+  const saveSettings = (e) => {
+    e.preventDefault();
+    setRefresh(!refresh);
+    setShowSettings(false);
+  };
+
   return (
     <main className="bg-white min-h-screen relative flex flex-col justify-center items-center py-10">
       <h1 className="text-center text-black text-3xl font-bold">
@@ -197,9 +256,13 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="absolute top-0 left-0 p-5 bg-slate-400 rounded-br-[10px] cursor-pointer hover:bg-slate-400/90">
+      <div
+        className="absolute top-0 left-0 p-5 bg-slate-400 rounded-br-[10px] cursor-pointer hover:bg-slate-400/90"
+        onClick={() => setShowSettings(true)}
+      >
         <BsFillGearFill className="text-[30px]" />
       </div>
+      {showSettings && <Settings saveSettings={saveSettings} />}
     </main>
   );
 }
