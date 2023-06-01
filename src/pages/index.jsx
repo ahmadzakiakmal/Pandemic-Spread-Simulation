db74ba7063caeca6cf7a0e17608cac115a6092ac
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-01 10:20:50 +0700

    feat: add disease spread functionality

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,40 +6,10 @@ function GridBox({ value, showVisuals, index, grid, cols }) {
   return (
     <div
       className={
-        "w-[80px] !aspect-square font-bold grid place-items-center border-black border-1 border box-border border-dashed " +
-        (grid[index - 1]?.value == 2 &&
-        index % cols !== 0 &&
-        (index - 1) % cols !== cols - 1
-          ? " bg-red-500/20 "
-          : "") +
-        (grid[index + 1]?.value == 2 &&
-        index % cols !== cols - 1 &&
-        (index + 1) % cols !== 0
-          ? " bg-red-500/20 "
-          : "") +
-        (grid[index - cols]?.value == 2 ? " bg-red-500/20 " : "") +
-        (grid[index + cols]?.value == 2 ? " bg-red-500/20 " : "") +
-        (grid[index - cols - 1]?.value == 2 &&
-        index % cols !== 0 &&
-        (index - cols - 1) % cols !== cols - 1
-          ? " bg-red-500/20 "
-          : "") +
-        (grid[index - cols + 1]?.value == 2 &&
-        index % cols !== cols - 1 &&
-        (index - cols + 1) % cols !== 0
-          ? " bg-red-500/20 "
-          : "") +
-        (grid[index + cols - 1]?.value == 2 &&
-        index % cols !== 0 &&
-        (index + cols - 1) % cols !== cols - 1
-          ? " bg-red-500/20 "
-          : "") +
-        (grid[index + cols + 1]?.value == 2 &&
-        index % cols !== cols - 1 &&
-        (index + cols + 1) % cols !== 0
-          ? " bg-red-500/20 "
-          : "") +
-        (value == 2 ? " bg-red-500/30 " : "")
+        "w-[80px] !aspect-square relative font-bold grid place-items-center border-black border-1 border box-border border-dashed " +
+        (value == 2
+          ? " bg-red-500/30 after:absolute after:w-[240px] after:aspect-square after:bg-red-500/[.15] "
+          : "")
       }
     >
       {showVisuals ? (
@@ -50,7 +20,7 @@ function GridBox({ value, showVisuals, index, grid, cols }) {
             <IoPerson
               className={
                 "text-[32px] " +
-                (value == 1 ? "text-green-600" : "text-red-500")
+                (value == 1 ? "text-green-600" : "text-red-800")
               }
             />
           </>
@@ -72,13 +42,15 @@ export default function Home() {
 
   // * Other Parameters
   // ? Rasio orang dengan total grid / Kepadatan populasi di area - Dimodelkan dengan probabilitas adanya orang dalam tiap grid
-  const [populationDensity, setPopulationDensity] = useState(0.3);
+  const [populationDensity, setPopulationDensity] = useState(0.5);
   // ? Rasio orang yang terinfeksi dengan total orang - Dimodelkan dengan probabilitas 1 orang membawa penyakit
   const [infectionRate, setInfectionRate] = useState(0.1);
   // ? Ukuran seberapa menular penyakit - Dimodelkan dengan jarak neighborhood yang dapat tertular
   const [spreadRange, setSpreadRange] = useState(1);
   // ? Imunitas seseorang - Dimodelkan dengan probabilitas orang dalam jarak tertular, bisa tidak tertular
   const [immunityRate, setImmunityRate] = useState(0.4);
+  // ? Iterasi waktu
+  const [iteration, setIteration] = useState(0);
 
   // * Variables for Visual Needs
   const [showVisuals, setShowVisuals] = useState(true);
@@ -108,12 +80,85 @@ export default function Home() {
     setGrid(newGrid);
   }, [rows, cols, refresh]);
 
+  // * Spread Disease
+  useEffect(() => {
+    if (grid.length > 0) {
+      const newGrid = [...grid];
+      for (let i = 0; i < rows * cols; i++) {
+        if (newGrid[i].value == 2) {
+          // * Spread Disease to the left of the sick person
+          if (
+            grid[i - 1] &&
+            grid[i - 1].value == 1 &&
+            i % cols != 0 //? Check if the person is not at the leftmost of the grid
+          ) {
+            newGrid[i - 1].value = 2;
+          }
+          // * Spread Disease to the right of the sick person
+          if (
+            grid[i + 1] &&
+            grid[i + 1].value == 1 &&
+            i % cols != cols - 1 //? Check if the person is not at the rightmost of the grid
+          ) {
+            newGrid[i + 1].value = 2;
+          }
+          // * Spread Disease to the top of the sick person
+          if (grid[i - cols] && grid[i - cols].value == 1) {
+            newGrid[i - cols].value = 2;
+          }
+          // * Spread Disease to the bottom of the sick person
+          if (grid[i + cols] && grid[i + cols].value == 1) {
+            newGrid[i + cols].value = 2;
+          }
+          // * Spread Disease to the top left of the sick person
+          if (
+            grid[i - cols - 1] &&
+            grid[i - cols - 1].value == 1 &&
+            i % cols != 0 //? Check if the person is not at the leftmost of the grid
+          ) {
+            newGrid[i - cols - 1].value = 2;
+          }
+          // * Spread Disease to the top right of the sick person
+          if (
+            grid[i - cols + 1] &&
+            grid[i - cols + 1].value == 1 &&
+            i % cols != cols - 1 //? Check if the person is not at the rightmost of the grid
+          ) {
+            newGrid[i - cols + 1].value = 2;
+          }
+          // * Spread Disease to the bottom left of the sick person
+          if (
+            grid[i + cols - 1] &&
+            grid[i + cols - 1].value == 1 &&
+            i % cols != 0 //? Check if the person is not at the leftmost of the grid
+          ) {
+            newGrid[i + cols - 1].value = 2;
+          }
+          // * Spread Disease to the bottom right of the sick person
+          if (
+            grid[i + cols + 1] &&
+            grid[i + cols + 1].value == 1 &&
+            i % cols != cols - 1 //? Check if the person is not at the rightmost of the grid
+          ) {
+            newGrid[i + cols + 1].value = 2;
+          }
+        }
+      }
+      setGrid(newGrid);
+    }
+  }, [iteration]);
+
   return (
     <main className="bg-white min-h-screen relative flex flex-col justify-center items-center py-10">
-      <h1 className="text-center text-black text-3xl font-bold">Disease Spread Simulation</h1>
+      <h1 className="text-center text-black text-3xl font-bold">
+        Disease Spread Simulation
+      </h1>
+      <h2 className="text-center text-black text-2xl font-bold">
+        Iteration {iteration}
+      </h2>
       <div className="flex flex-col justify-center items-center gap-10 mt-5">
         <div
-          className="simulation-grid w-fit outline outline-1 outline-black"
+          className="simulation-grid w-fit outline outline-1 outline-black !overflow-hidden"
           style={{ "--grid-cols": cols }}
         >
           {grid.map((grid_item, i) => (
@@ -127,13 +172,23 @@ export default function Home() {
             />
           ))}
         </div>
-        <div
-          className="bg-black text-white cursor-pointer p-3 unselectable"
-          onClick={() => {
-            setRefresh(!refresh);
-          }}
-        >
-          Regenerate
+        <div className="flex gap-5">
+          <div
+            className="bg-black text-white cursor-pointer p-3 unselectable"
+            onClick={() => {
+              setRefresh(!refresh);
+            }}
+          >
+            Regenerate
+          </div>
+          <div
+            className="bg-black text-white cursor-pointer p-3 unselectable"
+            onClick={() => {
+              setIteration(iteration + 1);
+            }}
+          >
+            Next Iteration
+          </div>
         </div>
       </div>
     </main>
